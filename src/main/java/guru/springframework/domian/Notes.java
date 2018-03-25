f31389f71f18ab33efe6b3163e69161488af15a9
roundrobine <roundrobine@gmail.com>
2018-03-25 14:46:53 +0200

    crete the Notes and Recipe entities and make them JPA aware

---
+++ src/main/java/guru/springframework/domian/Notes.java
@@ -0,0 +1,41 @@
+package guru.springframework.domian;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String notes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
